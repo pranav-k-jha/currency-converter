fb8c820ca7c741e9856c5f6a88e38b97e176fb1a
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-13 10:42:38 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import CurrencyRow from "./CurrencyRow"
+
+const App = () => {
+  return (
+    <>
+    <h1>Covert</h1>
+    <CurrencyRow/>
+    <div>=</div>
+    <CurrencyRow/>
+    </>
+  )
+}
+
+export default App
---
+++ src/CurrencyRow.jsx
@@ -0,0 +1,14 @@
+
+
+const CurrencyRow = () => {
+  return (
+    <div>
+        <input type="number" />
+        <select>
+            <option value="Hi">Hi</option>
+        </select>
+    </div>
+  )
+}
+
+export default CurrencyRow
