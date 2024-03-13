304a02e151ee8021118369bc863008b8fc0b9c72
Pranav Jha <pranav.jha@mail.concordia.ca>
2024-03-13 20:40:38 +0000

    loaded api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+
 import "./App.css";
 import CurrencyRow from "./CurrencyRow.jsx";
 
+const BASE_URL = "https://open.er-api.com/v6/latest/USD";
+
 const App = () => {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  console.log(currencyOptions)
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => 
+        setCurrencyOptions([data.base_codes, ...Object.keys(data.rates)])
+);}, []);
+
   return (
     <>
       <h1>Convert</h1>
